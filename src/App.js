fcf9c37f2cca3379809673e71359a91b53391b8c
thecoder1234-ssvbhilai <urwithom@gmail.com>
2023-03-18 17:10:47 +0530

    Part 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
-  c = 'John'
+
   render() {
     return (
       <div>
-        Hello my first class {this.c}
+        <Navbar />
+        <News />
       </div>
     )
   }
