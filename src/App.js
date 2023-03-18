9a7555514870a2027a66adba237c722b992a3571
thecoder1234-ssvbhilai <urwithom@gmail.com>
2023-03-18 17:52:59 +0530

    Part 3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar/>
         <News />
       </div>
     )
--- src/components/News.js
+++ src/components/News.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  render() {
-    return (
-      <div>
-        This is news
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className='container my-3'>
+                <h2>The True News - Top Headlines</h2>
+                <div className="row">
+                    <div className="col-md-4">
+                        <NewsItem title="title" description="desc" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="title" description="desc" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="title" description="desc" />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default News
