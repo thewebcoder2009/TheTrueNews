c158035c960b59f1ca675510855e93301c8aa65e
thecoder1234-ssvbhilai <urwithom@gmail.com>
2023-03-22 14:53:47 +0530

    Part 9

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route
+} from 'react-router-dom';
 
 export default class App extends Component {
-
-  render() {
-    return (
-      <div>
-        <Navbar/>
-        <News pageSize={5} country="in" category="science" />
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<Router>
+					<Navbar />
+					<Routes>
+						<Route exact path='/' element={<News key="general" pageSize={5} country="in" category="general" />} />
+						<Route exact path='/general' element={<News key="general" pageSize={5} country="in" category="general" />} />
+						<Route exact path='/health' element={<News key="health" pageSize={5} country="in" category="health" />} />
+						<Route exact path='/business' element={<News key="business" pageSize={5} country="in" category="business" />} />
+						<Route exact path='/sports' element={<News key="sports" pageSize={5} country="in" category="sports" />} />
+						<Route exact path='/tech' element={<News key="technology" pageSize={5} country="in" category="technology" />} />
+					</Routes>
+				</Router>
+			</div>
+		)
+	}
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,35 @@
 // import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">The True News</a>
+                    <Link className="navbar-brand" to="/">The True News</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a className="nav-link" aria-current="page" href="/">Home</a>
+                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/about">About</a>
+                                <Link className="nav-link" to="/health">Health</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/about">Business</a>
+                                <Link className="nav-link" to="/business">Business</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/about">Sports</a>
+                                <Link className="nav-link" to="/sports">Sports</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/about">Entertainment</a>
+                                <Link className="nav-link" to="/tech">Technology</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/about">Tech</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/about">General</a>
+                                <Link className="nav-link" to="/general">General</Link>
                             </li>
                         </ul>
                     </div>
